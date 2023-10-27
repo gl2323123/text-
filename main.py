7fac94218fa75749d988bc9033d98419987e6c81
GL~ <gleblobacev07@gmail.com>
2023-10-27 18:32:10 +0300

    Commit2

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 cook_book = {
-  'Омлет': [
-    {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
-    {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
-    {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
+    'Омлет': [
+        {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
+        {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл.'},
+        {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт.'}
     ],
-  'Утка по-пекински': [
-    {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
-    {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
-    {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
-    {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
+    'Утка по-пекински': [
+        {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт.'},
+        {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л.'},
+        {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л.'},
+        {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл.'}
     ],
-  'Запеченный картофель': [
-    {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
-    {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
-    {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г'},
+    'Запеченный картофель': [
+        {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг.'},
+        {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч.'},
+        {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г.'}
     ]
-  }
+}
 
 def get_shop_list_by_dishes(dishes, person_count):
     shop_list = {}
@@ -24,22 +24,21 @@ def get_shop_list_by_dishes(dishes, person_count):
             new_shop_list_item = dict(ingredient)
 
             new_shop_list_item['quantity'] *= person_count
-            if new_shop_list_item['ingridient_name'] not in shop_list:
-                shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
+            if new_shop_list_item['ingredient_name'] not in shop_list:
+                shop_list[new_shop_list_item['ingredient_name']] = new_shop_list_item
             else:
-                shop_list[new_shop_list_item['ingridient_name']]['quantity'] += new_shop_list_item['quantity']
+                shop_list[new_shop_list_item['ingredient_name']]['quantity'] += new_shop_list_item['quantity']
     return shop_list
+
 def print_shop_list(shop_list):
   for shop_list_item in shop_list.values():
-    print('{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['quantity'],
+    print('{} {} {}'.format(shop_list_item['ingredient_name'], shop_list_item['quantity'],
                             shop_list_item['measure']))
 
 def create_shop_list():
-  person_count = int(input('Введите количество человек: '))
-  dishes = input('Введите блюда в расчете на одного человека (через запятую): ') \
-    .lower().split(', ')
-  shop_list = get_shop_list_by_dishes(dishes, person_count)
-  print_shop_list(shop_list)
+    person_count = int(input('Введите количество человек: '))
+    dishes = input('Блюда, через запятую: ').split(', ')
+    shop_list = get_shop_list_by_dishes(dishes, person_count)
+    print_shop_list(shop_list)
 
 create_shop_list()
-
