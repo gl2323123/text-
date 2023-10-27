d4537fd0a9efc39d186c1a725ac3ef998b243ec1
GL~ <gleblobacev07@gmail.com>
2023-10-27 19:03:11 +0300

    Commit3

--- main.py
+++ main.py
@@ -1,44 +1,6 @@
-cook_book = {
-    'Омлет': [
-        {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
-        {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл.'},
-        {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт.'}
-    ],
-    'Утка по-пекински': [
-        {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт.'},
-        {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л.'},
-        {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л.'},
-        {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл.'}
-    ],
-    'Запеченный картофель': [
-        {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг.'},
-        {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч.'},
-        {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г.'}
-    ]
-}
-
-def get_shop_list_by_dishes(dishes, person_count):
-    shop_list = {}
-    for dish in dishes:
-        for ingredient in cook_book[dish]:
-            new_shop_list_item = dict(ingredient)
-
-            new_shop_list_item['quantity'] *= person_count
-            if new_shop_list_item['ingredient_name'] not in shop_list:
-                shop_list[new_shop_list_item['ingredient_name']] = new_shop_list_item
-            else:
-                shop_list[new_shop_list_item['ingredient_name']]['quantity'] += new_shop_list_item['quantity']
-    return shop_list
-
-def print_shop_list(shop_list):
-  for shop_list_item in shop_list.values():
-    print('{} {} {}'.format(shop_list_item['ingredient_name'], shop_list_item['quantity'],
-                            shop_list_item['measure']))
-
-def create_shop_list():
-    person_count = int(input('Введите количество человек: '))
-    dishes = input('Блюда, через запятую: ').split(', ')
-    shop_list = get_shop_list_by_dishes(dishes, person_count)
-    print_shop_list(shop_list)
-
-create_shop_list()
+filenames = ['gl1.txt', 'gl2.txt']
+with open('filenames', 'w') as outfile:
+    for fname in filenames:
+        with open(fname) as infile:
+            for line in infile:
+                outfile.write(line)
